f59ebb4adf7bc412ed459c0b10ffc95f73c46635
lily.goli <leilygoli100@yahoo.com>
2020-04-14 19:04:08 +0430

    database change

--- SAD/SAD/settings.py
+++ SAD/SAD/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'SAD.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'SAD_db2',
-            'USER': 'postgres',
-            'PASSWORD': '144916641881',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    # 'default': {
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': 'SAD_db2',
+    #         'USER': 'postgres',
+    #         'PASSWORD': '',
+    #         'HOST': '127.0.0.1',
+    #         'PORT': '5432',
+    #     }
 }
 
 
