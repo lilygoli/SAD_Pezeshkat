7db7e531e2c0ed0d854d5dd0fe3727dfe23e5d77
leily goli <leilygoli100@yahoo.com>
2020-05-01 02:17:47 +0430

    Add default_settings (for default django sqlite database).

--- SAD/SAD/settings.py
+++ SAD/SAD/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .settings_local import *
-
+try:
+    from .settings_local import *
+except ImportError:
+    from .settings_default import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
