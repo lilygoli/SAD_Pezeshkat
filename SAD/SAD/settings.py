dce178984f17c0e0724d2a27c494517b27336337
AradM <arad.mohammadi99@gmail.com>
2020-05-15 18:58:36 +0430

    fix heroku

--- SAD/SAD/settings.py
+++ SAD/SAD/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '5!fv(cj7)__=y$nrw6shvp$27madqt_5g4syi8dw85u#ow(v@4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com']
 
 # Application definition
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,13 +118,23 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
-STATICFILES_DIRS = [STATIC_DIR, os.path.join(BASE_DIR, 'static', 'static_dirs'),]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
+# STATICFILES_DIRS = [STATIC_DIR, os.path.join(BASE_DIR, 'static', 'static_dirs'),]
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = '/'
 AUTH_USER_MODEL = 'accounts.User'
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+
+import dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 django_heroku.settings(locals())
