b608a0e6a0b4aa49d97c205152e5eb27e7222edb
AradM <arad.mohammadi99@gmail.com>
2020-05-15 11:56:13 +0430

    change wsgi

--- SAD/SAD/wsgi.py
+++ SAD/SAD/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SAD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SAD.SAD.settings')
 
 application = get_wsgi_application()
