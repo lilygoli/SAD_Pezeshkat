5688209e54b0213c8310eb459a69afcd63df13d7
fereshtah <fereshteh.forghani2012@gmail.com>
2020-05-15 22:48:38 +0430

    make a simple view for doctor weekly schedule

--- SAD/doctor_calendar/urls.py
+++ SAD/doctor_calendar/urls.py
@@ -5,4 +5,5 @@ app_name = 'doctor_calendar'
 urlpatterns = [
     url(r'^calendar/(?P<pk>\d+)$', v.CalendarView.as_view(), name='calendar'),
     # url(r'^doctor-schedule/', v., name='schedule'),
+    url(r'^schedule/', v.DoctorCalenderView.as_view(), name='schedule')
 ]
