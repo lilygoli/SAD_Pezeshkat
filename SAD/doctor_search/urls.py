99783d21025296320a697ca2d17c33c9be6c6beb
fereshte forghani <fereshteh.forghani2012@gmail.com>
2020-05-16 17:12:40 +0000

    Revert "search"
    This reverts commit 415cdd0f87b65a7a7581658bcfd1b681d9057c30

--- SAD/doctor_search/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from doctor_search import views as v
-
-app_name = 'doctor_search'
-urlpatterns = [
-    url(r'^search_result/', v.SearchResultsView.as_view(), name='search_results'),
-    url(r'^search_page/', v.HomePageView.as_view(), name='search_page'),
-]
