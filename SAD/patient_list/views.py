47280f4ba31550fda1f22872cc1278373a57fe91
fereshtah <fereshteh.forghani2012@gmail.com>
2020-06-05 20:44:49 +0430

    patient list search

--- SAD/patient_list/views.py
+++ SAD/patient_list/views.py
@@ -7,12 +7,10 @@ from doctor_calendar.models import Event
 class PatientListView(ListView):
     template_name = 'patient_list/list.html'
     patients = None
-    patient_info = None
 
     def get_queryset(self):
         patients = Event.objects.filter(doctor_user=self.request.user.pk)
         self.patients = PatientProfileInfo.objects.none()
-        # self.patients_info = PatientProfileInfo.objects.none()
         for i in patients:
             self.patients |= PatientProfileInfo.objects.filter(user=i.patient_user)
 
