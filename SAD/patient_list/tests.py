5de14b09aaef925af2d771619ab2cd756ccc871d
fereshtah <fereshteh.forghani2012@gmail.com>
2020-06-06 12:44:24 +0430

    improve patient_list test

--- SAD/patient_list/tests.py
+++ SAD/patient_list/tests.py
@@ -26,15 +26,25 @@ class PatientListTest(TestCase):
         test_event = Event.objects.create(doctor_user=test_user1, patient_user=test_user2, start_time='2020-10-10', start_hour=10)
         test_event.save()
 
+        test_user3 = User.objects.create_user(email='feri@gmail.com', is_doctor=False, name="feri",
+                                              password='feri1234')
+        test_user3_profile = PatientProfileInfo.objects.create(user=test_user3, user_id=3, birthday='2010-10-10',
+                                                               medical_emergency_contact="09121211213")
+        test_user3.save()
+        test_user3_profile.save()
+
+        test_event2 = Event.objects.create(doctor_user=test_user1, patient_user=test_user3, start_time='2020-10-10',
+                                          start_hour=10)
+        test_event2.save()
+
     def test_list(self):
         url = '{url}'.format(
             url=reverse('patient_list:list'))
         self.client.login(username='aliii@gmail.com', password='ali12345')
         response = self.client.get(url)
-        # print("response",response)
-        # print("1", response.context['patients'])
-        # print("2", Event.objects.all())
-        self.assertQuerysetEqual(response.context['patients'], PatientProfileInfo.objects.all(), transform=lambda x: x)
+        print(response.context['patients'])
+        print(PatientProfileInfo.objects.all().order_by('id'))
+        self.assertQuerysetEqual(response.context['patients'], PatientProfileInfo.objects.all(), transform=lambda x: x, ordered=False)
 
 
 
